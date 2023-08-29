aa85a9baad5e05332c79d30a82ec00cf1249c258
0svaldojunior <app.o.r.jr@gmail.com>
2023-08-28 21:14:01 -0300

    chore: change .eslint and add prettier.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280',
+        'desktop-fullHD': '1920',
+        'desktop-quadHD': '2560',
+        'desktop-4k': '3840',
+      },
+
+      colors: {
+        junior: '#8257e6',
+      },
+
+      fontSize: {
+        '10xl': [
+          '10rem',
+          {
+            lineHeight: '1rem',
+          },
+        ],
       },
     },
   },
