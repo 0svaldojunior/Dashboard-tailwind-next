1c9aa1ded348eba19b316e5e64216587917b7981
0svaldojunior <app.o.r.jr@gmail.com>
2023-08-31 22:34:05 -0300

    feat: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,14 +16,18 @@ import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <h1 className="text-3xl font-medium text-zinc-900 dark:text-zinc-100">
+        Settings
+      </h1>
 
       <SettingsTabs />
 
       <div className="mt-6 flex flex-col">
-        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+        <div className="flex flex-col justify-between gap-4 border-b border-zinc-200 pb-5 dark:border-zinc-700 lg:flex-row lg:items-center">
           <div className="space-y-1">
-            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <h2 className="text-lg font-medium text-zinc-900 dark:text-zinc-100">
+              Personal info
+            </h2>
             <span className="text-sm text-zinc-500">
               Update your photo and personal details here.
             </span>
@@ -40,31 +44,40 @@ export default function Home() {
 
         <form
           id="settings"
-          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200 dark:divide-zinc-700"
         >
-          <div className="grid grid-cols-form gap-3">
+          <div className="flex flex-col gap-3 lg:grid lg:grid-cols-form">
             <label
               htmlFor="firstName"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Name
             </label>
-            <div className="grid grid-cols-2 gap-6">
+            <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2">
               <InputRoot>
-                <InputControl id="firstName" defaultValue="Osvaldo" />
+                <InputControl id="firstName" placeholder="Your name" />
               </InputRoot>
 
-              <InputRoot>
-                <InputControl defaultValue="Júnior" />
-              </InputRoot>
+              <div className="flex flex-col gap-3 lg:block">
+                <label
+                  htmlFor="lastName"
+                  className="text-sm font-medium text-zinc-700 dark:text-zinc-300 lg:sr-only"
+                >
+                  Last name
+                </label>
+
+                <InputRoot>
+                  <InputControl placeholder="Your sur name" />
+                </InputRoot>
+              </div>
             </div>
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
             <label
               htmlFor="email"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Email
             </label>
@@ -75,16 +88,16 @@ export default function Home() {
               <InputControl
                 id="email"
                 type="email"
-                defaultValue="app.o.r.jr@gmail.com"
+                placeholder="your@email.com"
               />
             </InputRoot>
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
             <label
               htmlFor="photo"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Your photo
               <span className="mt-0.5 block text-sm font-normal text-zinc-500">
@@ -92,7 +105,7 @@ export default function Home() {
               </span>
             </label>
 
-            <FileInputRoot className="flex items-start gap-5">
+            <FileInputRoot className="flex flex-col gap-5 lg:flex-row lg:items-start">
               <FileInputImagePreview />
               <FileInputTrigger />
               <FileInputControl />
@@ -100,20 +113,23 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
-            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="role"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
               Role
             </label>
             <InputRoot>
-              <InputControl id="role" defaultValue="CTO" />
+              <InputControl id="role" pattern="role" placeholder="Your role" />
             </InputRoot>
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
             <label
               htmlFor="country"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Country
             </label>
@@ -124,10 +140,10 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
             <label
               htmlFor="timezone"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Timezone
             </label>
@@ -141,15 +157,18 @@ export default function Home() {
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
-            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="bio"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
               Bio
               <span className="mt-0.5 block text-sm font-normal text-zinc-500">
                 Write a short introduction.
               </span>
             </label>
             <div className="space-y-3">
-              <div className="grid grid-cols-2 gap-3">
+              <div className="flex flex-col gap-3 lg:grid lg:grid-cols-2">
                 <Select placeholder="" defaultValue="normal">
                   <SelectItem
                     value="normal"
@@ -187,16 +206,16 @@ export default function Home() {
 
               <Textarea
                 id="bio"
-                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+                placeholder="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
               />
             </div>
             <div />
           </div>
 
-          <div className="grid grid-cols-form gap-3 pt-5">
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
             <label
               htmlFor="portfolioProjects"
-              className="text-sm font-medium text-zinc-700"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
             >
               Portfolio projects
               <span className="mt-0.5 block text-sm font-normal text-zinc-500">
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -9,16 +9,17 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
         alt=""
       />
+
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Osvaldo Junior
         </span>
-        <span className="truncate text-sm text-zinc-500">
-          o.r.jr@hotmail.com
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          o.r.jr@hotmail.comfeknbdmsfanbvm ,zmklnmkbdknlsdfbvkcn
         </span>
       </div>
 
-      <Button type="button" variant="ghost">
+      <Button type="button" variant="ghost" className="ml-14">
         <LogOut className="h-5 w-5 text-zinc-500" />
       </Button>
     </div>
